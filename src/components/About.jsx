66147f0ae0a4331d77b35bfaf495bbc78a25a949
hjmccain <hannahjmccain@gmail.com>
2017-05-31 12:15:23 -0400

    Add project outline to Readme.

---
+++ src/components/About.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>About!</h2>
+        </div>
+        <p className="App-intro">
+          Hannah's Create-React-App & Node.js starter files.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default About;
---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Welcome!</h2>
+        </div>
+        <p className="App-intro">
+          Hannah's Create-React-App & Node.js starter files.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
