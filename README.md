ae5f2cc58369bc71d57a8ea6be9d708f7419428a
hjmccain <hannahjmccain@gmail.com>
2017-05-31 13:11:56 -0400

    Add initial oauth config.

--- README.md
+++ README.md
@@ -4,6 +4,6 @@
 App outline:
 - App is linked to a Google account.
 - Whenever a new Google calendar event is made, it gets queued on app. User gets to choose which events to import into app.
-- If an event is imported, it comes with a date-created timestamp (assuming that's on the event object...)
+- If an event is imported, it comes with a date-created timestamp (object.created)
 - Each event has date created, date of appointment, description, quote amt, client info (name, contact), payment amt (entered after tattoo, automatically adjusted for Angie's cut, but can be manually updated on top of that), tip amt, ability to take photo to associate with appt & client
 - All clients are stored in database, and if name & phone number match, they're stored as a single client
--- server/app.js
+++ server/app.js
@@ -1,11 +1,34 @@
 const express = require('express');
 const path = require('path');
+const GoogleStrategy = require('passport-google-oauth20');
+const passport = require('passport');
+const dotenv = require('dotenv');
+let currentUser;
+
+const useOrUpdate = (id) => {
+  if (id !== currentUser) { currentUser = id };
+  console.log(currentUser);
+}
+
+passport.use(new GoogleStrategy({
+    clientID: CLIENT_ID,
+    clientSecret: CLIENT_SECRET,
+    callbackURL: "http://localhost:8080/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    useOrUpdate({ googleId: profile.id })
+    .then((user) => {
+      callback(null, user)
+    }).catch((err) => {
+      console.error(err);
+    });
+  }
+));
 
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, '..', 'build')));
 
-// Always return the main index.html, so react-router renders the route in the client
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
 });
